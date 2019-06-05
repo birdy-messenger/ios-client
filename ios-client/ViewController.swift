46b4ff8827cca26ea510b51396f56251e14b40a8
Vsevolod Konyakhin <konyakhinvsevolod@mail.ru>
2019-06-05 15:46:59 +0300

    First commit

---
+++ ios-client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-client
+//
+//  Created by Владимир Коняхин on 05/06/2019.
+//  Copyright © 2019 Vsevolod Konyakhin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
